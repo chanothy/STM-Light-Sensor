733093dec5d4dbdf348c1618c6bebaaeb83a40ec
Chan, Tim <timchan@iu.edu>
2024-01-17 14:13:59 -0500

    build with attribute weak in syscalls

--- L432_project/Core/Src/syscalls.c
+++ L432_project/Core/Src/syscalls.c
@@ -89,27 +89,27 @@ __attribute__((weak)) int _write(int file, char *ptr, int len)
   return len;
 }
 
-int _close(int file)
+__attribute__((weak)) int _close(int file)
 {
   (void)file;
   return -1;
 }
 
 
-int _fstat(int file, struct stat *st)
+__attribute__((weak)) int _fstat(int file, struct stat *st)
 {
   (void)file;
   st->st_mode = S_IFCHR;
   return 0;
 }
 
-int _isatty(int file)
+__attribute__((weak)) int _isatty(int file)
 {
   (void)file;
   return 1;
 }
 
-int _lseek(int file, int ptr, int dir)
+__attribute__((weak)) int _lseek(int file, int ptr, int dir)
 {
   (void)file;
   (void)ptr;
