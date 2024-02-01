924e941e70fe8a537393d6d9893f8c77c821c7a4
Chan, Tim <timchan@iu.edu>
2024-01-31 19:44:52 -0500

    implementing command.c

--- L432_project/Core/Src/command.c
+++ L432_project/Core/Src/command.c
@@ -5,10 +5,28 @@
 #include <main.h>
 #include <string.h>
 
+void help_command(char *);
+void lof_command(char *);
+void lon_command(char *);
+
 void prompt() {
 	printf("> ");
 }
-void help_command() {
+
+typedef struct command {
+	char * cmd_string;
+	void (*cmd_function)(char * arg);
+} command_t;
+
+command_t commands[] = {
+	{"help",help_command},
+	{"lon",lon_command},
+	{"lof",lof_command},
+	{0,0}
+};
+
+
+void __attribute__((weak)) help_command(char *arguments) {
 	printf("Available Commands:\n\r");
 	printf("help\n\r");
 	printf("lof\n\r");
@@ -19,28 +37,30 @@ void help_command() {
 void test_command(char *arguments) {
 
 }
-void lof_command() {
+
+void __attribute__((weak)) lof_command(char *arguments) {
 	printf("led_off\n\r");
 	HAL_GPIO_WritePin(LD3_GPIO_Port, LD3_Pin, 0);
 }
 
-void lon_command() {
+void __attribute__((weak)) lon_command(char *arguments) {
 	printf("led_on\n\r");
 	HAL_GPIO_WritePin(LD3_GPIO_Port, LD3_Pin, 1);
 }
+
 void get_command(uint8_t command[]) {
 	const char *led_on = "lon";
 	const char *led_off = "lof";
 	const char *help = "help";
 
 	if (strstr(command,led_on) != NULL) {
-	  lon_command();
+	  lon_command(0);
 	}
 	else if (strstr(command,led_off) != NULL) {
-	  lof_command();
+	  lof_command(0);
 	}
 	else if (strstr(command,help) != NULL) {
-	  help_command();
+	  help_command(0);
 	}
 	else {
 	  printf("invalid_command\n\r");
@@ -48,4 +68,57 @@ void get_command(uint8_t command[]) {
 	prompt();
 }
 
+int execute_command(uint8_t * line) {
+  uint8_t *cmd;
+  uint8_t *arg;
+  command_t *p = commands;
+  int success = 0;
+
+  if (!line) {
+    return (-1); // Passed a bad pointer
+  }
+  if (parse_command(line,&cmd,&arg) == -1) {
+    printf("Error with parse command\n\r");
+    return (-1);
+  }
+  while (p->cmd_string) {
+    if (!strcmp(p->cmd_string,(char *) cmd)) {
+      if (!p->cmd_function) {
+        return (-1);
+      }
+      (*p->cmd_function)((char *)arg);            // Run the command with the passed arguments
+      success = 1;
+      break;
+    }
+    p++;
+  }
+  if (success) {
+    return (0);
+  }
+  else {
+    return (-1);
+  }
+}
+
+int parse_command (uint8_t *line, uint8_t **command, uint8_t **args) {
+  uint8_t *p;
+
+  if((!line) ||
+     (!command) ||
+     (!args)) {
+    return (-1);
+  }
+  *command = line;
+  p = line;
+  while (*p!=','){
+    if (!*p) {
+      *args = '\0';
+      return(0);
+    }
+    p++;
+  }
+  *p++ = '\0';
+  *args = p;
+  return (0);
+}
 
